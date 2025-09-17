a5abdf4852902dff92b07e33fd2883dbbb84d337
Official <diegoferraro@free.fr>
2025-09-17 22:29:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// Menu déroulant
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-menuBtn.addEventListener("click", () => {
-  menu.style.display = menu.style.display === "block" ? "none" : "block";
+// Ouvrir / fermer le menu
+const menuToggle = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+menuToggle.addEventListener("click", () => {
+  sidebar.classList.add("active");
+});
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
 });
 
 // Tri des produits
@@ -21,3 +26,4 @@ sortSelect.addEventListener("change", () => {
 
   products.forEach(product => productsContainer.appendChild(product));
 });
+
